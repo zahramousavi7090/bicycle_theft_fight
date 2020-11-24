302efd8c46044ca2e6249e89db5f92f9287d727b
Zahra <zahramousavi7090@gmail.com>
2020-11-24 19:46:42 +0330

    fix delete police

--- src/main/java/com/bicyle_theft/demo/police/PoliceService.java
+++ src/main/java/com/bicyle_theft/demo/police/PoliceService.java
@@ -3,6 +3,7 @@ package com.bicyle_theft.demo.police;
 import com.bicyle_theft.demo.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -49,6 +50,7 @@ public class PoliceService {
         return policeRepository.save(currentPolice);
     }
 
+    @Transactional
     public void deleteByID(UUID id) {
         if (!policeRepository.existsById(id))
             throw new NotFoundException("this police Not Found");
